220e58ffc250e2768cd53582bc12ce70ccb1ff82
hank9999 <hank9999@qq.com>
2020-06-13 22:41:07 +0800

    update

--- src/main/java/com/github/hank9999/keywordblock/Libs/Lib.java
+++
@@ -1,9 +0,0 @@
-package com.github.hank9999.keywordblock.Libs;
-
-import org.bukkit.ChatColor;
-
-final public class Lib {
-    public static String color_translate(String text) {
-        return ChatColor.translateAlternateColorCodes('&', text);
-    }
-}
